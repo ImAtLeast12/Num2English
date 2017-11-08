9c42f3975f107a7eb11bd1830143a106000b425a
ImAtLeast12 <imatleast12@gmail.com>
2017-11-08 11:14:08 -0500

    Finished
    Might want to make the code look a bit nicer but that is nitpicking.

--- Num2English.py
+++ Num2English.py
@@ -8,7 +8,7 @@ ThroughTwenty =['ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen
 Tens = ['','','twenty','thirty','fourty','fifty','sixty','seventy','eighty', 'ninety']
 
 
-
+##This is all to find the fixed point form the vsauce video
 
 
 def sayTheNumber(number):
@@ -21,21 +21,21 @@ def sayTheNumber(number):
         tens = stringNum[1]
         hundreds = stringNum[2]
         #Then it is going to  be in the hundreds
-        output += (Ones[int(hundreds)] + ' hundred')
+        output += (Ones[int(hundreds)] + 'hundred')
 
         if (int(tens)!= 1):
-            output += (' ' + Tens[int(tens)] )
-            output += (' ' + Ones[int(ones)])
+            output += (Tens[int(tens)] )
+            output += (Ones[int(ones)])
         else:
-            output += (' '+ ThroughTwenty[int(ones)])
+            output += (ThroughTwenty[int(ones)])
         return output
 
     if(len(stringNum) == 2):
         ones = stringNum[0]
         tens = stringNum[1]
         if (int(tens)!= 1):
-            output += (' '+ Tens[int(tens)])
-            output += (' '+ Ones[int(ones)])
+            output += (Tens[int(tens)])
+            output += (Ones[int(ones)])
         else:
             output += (ThroughTwenty[int(ones)])
         return output
@@ -48,6 +48,10 @@ def sayTheNumber(number):
             output += ('zero')
         return output
 
-for i in range(99,199):
-    print(sayTheNumber(i))
+for n in range(0, 1000,2): #This only looks at 0 to 99
+    for i in range(0,1000): #adds a bunch of negatives which adds 8 for each one
+        x = sayTheNumber(i)
+        if(len(x)+(8*n) == i):
+            print(('negative' * n) + x)
+        
 #so this is working quite well the only thing is that I don't want to have to make a case for each of these when they get added it would be better if I could consolidate these somehow

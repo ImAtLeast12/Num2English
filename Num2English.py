9159436ad736630831f008d23788a49cdb43187e
ImAtLeast12 <imatleast12@gmail.com>
2017-11-10 12:22:55 -0500

    Changed the length of the functions
    This is so that I can change for different comma lengths 
    (ie Japanese)

--- Num2English.py
+++ Num2English.py
@@ -2,30 +2,30 @@ Ones = ['',' one',' two',' three',' four',' five',' six', ' seven', ' eight', ' 
 ThroughTwenty =[' ten',' eleven',' twelve',' thirteen',' fourteen',' fifteen',' sixteen',' seventeen',' eighteen',' nineteen']
 Tens = ['','',' twenty',' thirty',' fourty',' fifty',' sixty',' seventy',' eighty', ' ninety']
 place = ['',' thousand',' million',' billion',' trilion',' quadrillion',' quintillion',' sextillion',' octillion' ' nonillion', ' decillion', ' udecillion', ' duodecillion', ' tredecillion', ' quatturordecillion', ' quindecillion', ' sexdecillion', ' septendecillion', ' octodecillion', ' novemdecillion', ' vigintillion']
-def sayTheNumberOnes(number):
-    return Ones[int(number)]
-def sayTheNumberTens(number):
-    if (number == '00'): return ''
-    if (number[0] == '1'): return ThroughTwenty[int(number[1])]
-    else: return Tens[int(number[0])] + sayTheNumberOnes(number[1])
-def sayTheNumberHunds(number):
+def sayOnes(seg):
+    return Ones[int(seg)]
+def sayTens(seg):
+    if (seg == '00'): return ''
+    if (seg[0] == '1'): return ThroughTwenty[int(seg[1])]
+    else: return Tens[int(seg[0])] + sayOnes(seg[1])
+def sayHunds(seg):
     output = ''
-    if (number == '000'): return output
-    if (number[0] == '0'):return sayTheNumberTens(number[1:3]) 
-    else: return Ones[int(number[0])] + ' hundred' + sayTheNumberTens(number[1]+number[2])
-stn=[sayTheNumberHunds,sayTheNumberTens,sayTheNumberOnes]
+    if (seg == '000'): return output
+    if (seg[0] == '0'):return sayTens(seg[1:3]) 
+    else: return Ones[int(seg[0])] + ' hundred' + sayTens(seg[1:3])
+stn=[sayHunds,sayTens,sayOnes]
 def getCardinal(number,output,depth):
     number = str(number)
     if(number == ''):
         return output
-    for i in range(3):
+    for i in range(len(stn)):
         try:
-            sh = stn[i](number[-3+i:])
+            sh = stn[i](number[-len(stn)+i:])
             if(sh==''):
                 output += sh
             else:
                 output = sh + place[depth] + output
-            return getCardinal(number[:-3+i],output,depth+1)
+            return getCardinal(number[:-len(stn)+i],output,depth+1)
         except:
             pass
 for i in range(5):

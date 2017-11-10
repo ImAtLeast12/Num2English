7c747ee9800f86cb496c69ae366b754f59f8e310
ImAtLeast12 <imatleast12@gmail.com>
2017-11-10 12:04:55 -0500

    Cleaned the code up so it looks nice and purdy
    I got rid of redundancies and things that was legacy code Now I am probably going to change the names so it sounds better

--- Num2English.py
+++ Num2English.py
@@ -2,79 +2,31 @@ Ones = ['',' one',' two',' three',' four',' five',' six', ' seven', ' eight', ' 
 ThroughTwenty =[' ten',' eleven',' twelve',' thirteen',' fourteen',' fifteen',' sixteen',' seventeen',' eighteen',' nineteen']
 Tens = ['','',' twenty',' thirty',' fourty',' fifty',' sixty',' seventy',' eighty', ' ninety']
 place = ['',' thousand',' million',' billion',' trilion',' quadrillion',' quintillion',' sextillion',' octillion' ' nonillion', ' decillion', ' udecillion', ' duodecillion', ' tredecillion', ' quatturordecillion', ' quindecillion', ' sexdecillion', ' septendecillion', ' octodecillion', ' novemdecillion', ' vigintillion']
-
-def commas(number): #This determins the amout of times i need to find the number between a commas splice
-    return number//3
-
 def sayTheNumberOnes(number):
     return Ones[int(number)]
-
 def sayTheNumberTens(number):
-    output = ''    
-    if (number == '00'):
-        return output
-    number=number[::-1]
-    
-    if (number[1] == '1'):
-        output += ThroughTwenty[int(number[0])]
-    else:
-        output += Tens[int(number[1])] + sayTheNumberOnes(number[0])
-    return output
-
+    if (number == '00'): return ''
+    if (number[0] == '1'): return ThroughTwenty[int(number[1])]
+    else: return Tens[int(number[0])] + sayTheNumberOnes(number[1])
 def sayTheNumberHunds(number):
     output = ''
-    if (number == '000'):
-        return output
-    if (number[0] == '0'):
-        return sayTheNumberTens(number[1:3]) 
-    else:
-        return Ones[int(number[0])] + ' hundred' + sayTheNumberTens(number[1]+number[2])
-        
+    if (number == '000'): return output
+    if (number[0] == '0'):return sayTheNumberTens(number[1:3]) 
+    else: return Ones[int(number[0])] + ' hundred' + sayTheNumberTens(number[1]+number[2])
+stn=[sayTheNumberHunds,sayTheNumberTens,sayTheNumberOnes]
 def getCardinal(number,output,depth):
     number = str(number)
-
     if(number == ''):
         return output
-    
-    try:
-        sh = sayTheNumberHunds(number[-3:])
-        if(sh==''):
-            output += sh
-        else:
-            output = sh + place[depth] + output
-        return getCardinal(number[:-3],output,depth+1)
-    except:
-        pass
-
-    try:
-        sh = sayTheNumberTens(number[-2:])
-        if(sh==''):
-            output += sh
-        else:
-            output = sh + place[depth] + output
-        return getCardinal(number[:-2],output,depth+1)
-    except:
-        pass
-
-    try:
-        sh = sayTheNumberOnes(number[-1:])
-        if(sh==''):
-            output += sh
-        else:
-            output = sh + place[depth] + output
-        return getCardinal(number[:-1],output,depth+1)
-    except:
-        pass
-
-while True:
-    x = input()
-    if x == ('n' or 'N'):
-        print('ending program')
-        break
-    if x.isnumeric():
-        print(getCardinal(x,'',0))
-    else:
-        print('Input must be a number! type n to stop')
-        
-
-
+    for i in range(3):
+        try:
+            sh = stn[i](number[-3+i:])
+            if(sh==''):
+                output += sh
+            else:
+                output = sh + place[depth] + output
+            return getCardinal(number[:-3+i],output,depth+1)
+        except:
+            pass
+for i in range(5):
+    print(getCardinal(input(),'',0))
